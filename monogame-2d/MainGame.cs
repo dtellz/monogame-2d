7e326f6a758cd488f2a346ebcd667f6bd8406795
Diego <dtellez95@gmail.com>
2023-12-09 18:55:47 +0000

    fix bug rendering null exception

--- monogame-2d/MainGame.cs
+++ monogame-2d/MainGame.cs
@@ -38,16 +38,13 @@ public class MainGame : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        _graphics.PreferredBackBufferWidth = 1024;
+        _graphics.PreferredBackBufferHeight = 768;
+        _graphics.IsFullScreen = false;
+        _graphics.ApplyChanges();
 
-        _renderTarget = new RenderTarget2D(
-            _graphics.GraphicsDevice,
-            DESIGNED_RESOLUTION_WIDTH,
-            DESIGNED_RESOLUTION_HEIGHT,
-            false,
-            SurfaceFormat.Color,
-            DepthFormat.None,
-            0,
-            RenderTargetUsage.DiscardContents);
+        _renderTarget = new RenderTarget2D(_graphics.GraphicsDevice, DESIGNED_RESOLUTION_WIDTH, DESIGNED_RESOLUTION_HEIGHT, false,
+            SurfaceFormat.Color, DepthFormat.None, 0, RenderTargetUsage.DiscardContents);
 
         _renderScaleRectangle = GetScaleRectangle();
 
@@ -74,29 +71,28 @@ public class MainGame : Game
 
     protected override void Draw(GameTime gameTime)
     {
-        // TODO: Add your drawing code here
-
+        // Render to the Render Target
         GraphicsDevice.SetRenderTarget(_renderTarget);
+
         GraphicsDevice.Clear(Color.CornflowerBlue);
+
         _spriteBatch.Begin();
-        _currentGameState.Render(_spriteBatch);
+
+        if (_currentGameState != null)
+        {
+            _currentGameState.Render(_spriteBatch);
+        }
         _spriteBatch.End();
 
-        // render the scaled content
+        // Now render the scaled content
         _graphics.GraphicsDevice.SetRenderTarget(null);
-        _graphics.GraphicsDevice.Clear(
-            ClearOptions.Target,
-            Color.Black,
-            1.0f,
-            0);
 
-        _spriteBatch.Begin(
-            SpriteSortMode.Immediate,
-            BlendState.Opaque);
-        _spriteBatch.Draw(
-            _renderTarget,
-            _renderScaleRectangle,
-            Color.White);
+        _graphics.GraphicsDevice.Clear(ClearOptions.Target, Color.Black, 1.0f, 0);
+
+        _spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Opaque);
+
+        _spriteBatch.Draw(_renderTarget, _renderScaleRectangle, Color.White);
+
         _spriteBatch.End();
 
         base.Draw(gameTime);
@@ -113,12 +109,14 @@ public class MainGame : Game
         {
             var presentHeight = (int)(Window.ClientBounds.Width / DESIGNED_RESOLUTION_ASPECT_RATIO + variance);
             var barHeight = (Window.ClientBounds.Height - presentHeight) / 2;
+
             scaleRectangle = new Rectangle(0, barHeight, Window.ClientBounds.Width, presentHeight);
         }
         else
         {
             var presentWidth = (int)(Window.ClientBounds.Height * DESIGNED_RESOLUTION_ASPECT_RATIO + variance);
             var barWidth = (Window.ClientBounds.Width - presentWidth) / 2;
+
             scaleRectangle = new Rectangle(barWidth, 0, presentWidth, Window.ClientBounds.Height);
         }
 
