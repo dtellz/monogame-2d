ea790a69f2538f518ce0886eb387a8fff842c56c
Diego <dtellez95@gmail.com>
2023-12-28 12:39:32 +0000

    add bullet sprite class

---
+++ monogame-2d/Objects/BulletSprite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using monogame2d.Objects.Base;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace monogame2d.Objects
+{
+	public class BulletSprite : BaseGameObject
+	{
+		private const float BULLET_SPEED = 10.0f;
+
+		public BulletSprite(Texture2D texture)
+		{
+			_texture = texture;
+		}
+
+		public void MoveUp()
+		{
+			Position = new Vector2(Position.X, Position.Y - BULLET_SPEED);
+		}
+	}
+}
+
